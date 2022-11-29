c2fec7a54211494ba4a634468636a8f65d37b11f
MrNesli <57979201+MrNesli@users.noreply.github.com>
2022-11-29 01:19:19 +0100

    Removing escape characters, Filtering. Separated Color class in it's separate file.

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from os import system
+from time import sleep
+import re
 
 from src.window.frame import Frame, Window
+from src.window.color import Color
+from src.window.matrix import Matrix
 
-system('cls')  # Change to "clear" if you are not on windows
+system('clear') # Change to "clear" if you are not on windows
 
-map = Window('''\
-                   ||||       
-                   ||||       
-                     ─┬─┬─┬─┬─
-                              
-                              
-╭│╮   ╶─╮                     
-│││    ░│                     
-╰│╯   ╶─╯           ╭─────┬─╮ 
-                    │░░░░░│▓│ 
-                    ╰─────┴─╯ 
-''', name='BEDROOM')
+main = Window((40, 60))
+main.add_frame(Frame((6, 20)), (3, 3))
+main.add_frame(Frame((2, 2)))
 
-map.add_frame(Frame(size=(2, 9)), (4, 0))
+test_str = Matrix(f'''
+╭───────────╮
+│╭─╮        │
+││{Color.RED}a{Color.DEFAULT}│        │
+│╰─╯        │
+│   ╭─╮     │
+│   │b│     │
+│   ╰─╯     │
+│           │
+│           │
+│        ╭─╮│
+│        │c││
+│        ╰─╯│
+╰───────────╯
+''')
+
+# print(test_str)
+# test_str = f'{Color.RED} Hello world, {Color.BLUE} This is {Color.DEFAULT}'
+# print(test_str)
+# print(remove_and_save_escape_chars(test_str))
--- src/window/screen.py
+++ src/window/screen.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
-from colorama import Fore
-
+from src.window.color import Color
 from src.types_ import CoordReference
 from src.window.coord import Coord
 
@@ -19,30 +18,6 @@ class AnsiEscapeCode:
     def execute(self) -> None:
         """Execute the ANSI escape code."""
         print(self.code, end='')
-
-
-class Color:
-    NORMAL = Fore.RESET
-    DEFAULT = NORMAL
-    RESET = NORMAL
-
-    BLACK = Fore.BLACK                     # 0x000000
-    GRAY = Fore.LIGHTBLACK_EX              # 0x666666
-    BRIGHT_WHITE = Fore.LIGHTWHITE_EX      # 0xE5E5E5
-
-    BRIGHT_RED = Fore.LIGHTRED_EX          # 0xF14C4C
-    RED = Fore.RED                         # 0xCD3131
-    YELLOW = Fore.YELLOW                   # 0xE5E510
-    BRIGHT_YELLOW = Fore.LIGHTYELLOW_EX    # 0xF5F543
-    BRIGHT_GREEN = Fore.LIGHTGREEN_EX      # 0x23D18B
-    GREEN = Fore.GREEN                     # 0x0DBC79
-    CYAN = Fore.CYAN                       # 0x11A8CD
-    BRIGHT_CYAN = Fore.LIGHTCYAN_EX        # 0x29B8DB
-    BRIGHT_BLUE = Fore.LIGHTBLUE_EX        # 0x3B8EEA
-    BLUE = Fore.BLUE                       # 0x2472C8
-    MAGENTA = Fore.MAGENTA                 # 0xBC3FBC
-    BRIGHT_MAGENTA = Fore.LIGHTMAGENTA_EX  # 0xD670D6
-
 
 class Cursor:
     """https://en.wikipedia.org/wiki/ANSI_escape_code"""
